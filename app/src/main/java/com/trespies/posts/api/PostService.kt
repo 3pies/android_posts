7da193773bce655c0f757446d5ea2e679219b940
danielferrer <danybarco@gmail.com>
2020-05-04 07:41:10 +0200

    Add WS calls

--- app/src/main/java/com/trespies/posts/api/PostService.kt
+++ app/src/main/java/com/trespies/posts/api/PostService.kt
@@ -1,7 +1,9 @@
 package com.trespies.posts.api
 
 import androidx.lifecycle.LiveData
+import com.trespies.posts.api.objects.ApiObjectComment
 import com.trespies.posts.api.objects.ApiObjectPost
+import com.trespies.posts.api.objects.ApiObjectUser
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -14,4 +16,10 @@ interface PostService {
 
     @GET("posts")
     fun getPosts(): LiveData<ApiResponse<List<ApiObjectPost>>>
+
+    @GET("users/{userID}")
+    fun getUser(@Path("userID") userID: Int) : LiveData<ApiResponse<ApiObjectUser>>
+
+    @GET("posts/{postID}/comments")
+    fun getCommentsByPostID(@Path("postID") postID: Int) : LiveData<ApiResponse<List<ApiObjectComment>>>
 }
