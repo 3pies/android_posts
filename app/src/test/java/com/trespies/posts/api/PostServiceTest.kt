715a0b0284b88a786828d7b26b573e03719740f7
danielferrer <danybarco@gmail.com>
2020-05-05 21:39:47 +0200

    Add WS tests

--- app/src/test/java/com/trespies/posts/api/PostServiceTest.kt
+++ app/src/test/java/com/trespies/posts/api/PostServiceTest.kt
@@ -7,6 +7,7 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okio.Okio
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.After
 import org.junit.Assert.assertThat
 import org.junit.Before
@@ -67,6 +68,40 @@ class GithubServiceTest {
         val post3 = posts[2]
         assertThat(post3.id, `is`(3))
         assertThat(post3.userId, `is`(2))
+    }
+
+    @Test
+    fun getPostDetail() {
+        enqueueResponse("post_detail.json")
+
+        val post = (service.getPost(1).getOrAwaitValue() as ApiSuccessResponse).body
+
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path, `is`("/posts/1"))
+
+        assertThat(post, notNullValue())
+        assertThat(post.id, `is`(1))
+        assertThat(post.userId, `is`(2))
+        assertThat(post.title, `is`("title1"))
+        assertThat(post.body, `is`("body1"))
+    }
+
+    @Test
+    fun getUserDetail() {
+        enqueueResponse("user_detail.json")
+
+        val user = (service.getUser(1).getOrAwaitValue() as ApiSuccessResponse).body
+
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path, `is`("/users/1"))
+
+        assertThat(user, notNullValue())
+        assertThat(user.id, `is`(1))
+        assertThat(user.email, `is`("daniel.ferrer.delgado@gmail.com"))
+        assertThat(user.name, `is`("Daniel Ferrer"))
+        assertThat(user.username, `is`("ferrer"))
+        assertThat(user.company?.name, `is`("3pies.es"))
+        assertThat(user.address?.city, `is`("coruña"))
     }
 
     private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
